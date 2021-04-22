5f191ed78165fab2c803b039f82039068d5d3c5a
vidhya <vidhyav656@gmail.com>
2021-04-22 18:43:56 +0530

    update

--- js/grayscale.js
+++ js/grayscale.js
@@ -6,31 +6,50 @@
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+	if ($(".navbar").offset().top > 50) {
+		$(".navbar-fixed-top").addClass("top-nav-collapse");
+	} else {
+		$(".navbar-fixed-top").removeClass("top-nav-collapse");
+	}
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
 });
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+	$('.navbar-toggle:visible').click();
 });
 
 // remove the focused state after click,
 // otherwise bootstrap will still highlight the link
-$("a").mouseup(function(){
-    $(this).blur();
+$("a").mouseup(function() {
+	$(this).blur();
 })
+
+$(window).on('load', function() {
+	$.instagramFeed({
+		'username': 'dustinwmooney',
+		'container': "#instagram-feed1",
+		'display_profile': true,
+		'display_biography': true,
+		'display_gallery': true,
+		'display_captions': true,
+		'max_tries': 8,
+		'callback': null,
+		'styling': true,
+		'items': 8,
+		'items_per_row': 4,
+		'margin': 1,
+		'lazy_load': true,
+		'on_error': console.error
+	});
+});
